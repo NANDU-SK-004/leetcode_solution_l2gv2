013ca27f8b307e28ae1a929af151be26bd414229
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-08 18:21:44 +0530

    Sync: 217. Contains Duplicate (Easy) [Accepted] - Leet2Git v2.0

---
+++ Array/217_Contains_Duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash ={}
+
+        for num in nums:
+            hash[num] = hash.get(num ,1)+1
+        print(hash)
