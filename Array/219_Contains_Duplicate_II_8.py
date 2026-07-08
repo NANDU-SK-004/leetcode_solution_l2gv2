e9c70d368e0c0fa1d9c1eb3e27dcd8156ca84174
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-08 19:59:14 +0530

    Sync: 219. Contains Duplicate II (Easy) [Accepted] - Leet2Git v2.0

---
+++ Array/219_Contains_Duplicate_II_8.py
@@ -0,0 +1,34 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # hash ={}
+        # for num in nums:
+        #     hash[num] = hash.get(num ,0)+1
+        # arr =[]
+
+
+        # for key ,value in hash.items():
+        #     if value >1:
+        #         arr.append(key) 
+
+        # for val in arr:
+        #     if k >= len(nums):
+        #         if nums.count(val) > 1:
+        #             return True
+        #     else:
+        #         for i in range(len(nums)-k +1):
+                    
+        #             if nums[i:i+k+1].count(val) > 1:
+        #                 return True
+        # return False
+
+        if len(nums) == len(set(nums)):
+            return False
+
+        for i in range (len(nums)):
+            if nums[i] in nums[i+1 :i+k+1]:
+                return True
+        return False
+
+
+
+        
